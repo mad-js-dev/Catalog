f1793c6d08782f9d63016bd2ee95e27d819eb8f9
Brais <braisva@gmail.com>
2023-06-06 13:31:30 +0200

    .

--- index.js
+++ index.js
@@ -39,7 +39,20 @@ async function run() {
     var jsonParser = bodyParser.json()
     const port = process.env.PORT || 80;
     
-    app.use(cors())
+    app.use(function(req, res, next) {  
+      res.header('Access-Control-Allow-Origin', 'http://local:8001');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+      res.header(
+        'Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-Api-Key'
+      );
+      res.header('Access-Control-Allow-Credentials', 'true');
+      if ('OPTIONS' === req.method) {
+        res.sendStatus(200);
+      }
+      else {
+        next();
+      }
+    });
     
     
 
